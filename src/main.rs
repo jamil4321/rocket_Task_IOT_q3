04ec3d8d71403cf2cd4ee352d7a91d943f2b861d
Jamil Ahmed <jamil.ahmed.ja28@gmail.com>
2020-05-31 20:04:34 +0500

    task compelet

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use rocket::request::Form;
+use rocket::response::NamedFile;
+
+
+// Create Struct to get data from html form
+#[derive(Debug, FromForm)]
+struct Task {
+     num: u32,
+}
+
+
+// creat testPost function for post request
+// where Path is Index or "/" and data = prametor which we pass in function
+// parameter task data type is Task Struct and pass it through Rocket From
+// this function is returning a string from function
+#[post("/", data = "<task>")]
+fn testPost(task: Form<Task>)->String{
+    format!("Number is = {} after add 50 = {}",task.num,task.num + 50)
+}
+
+// creat index function for get request
+//where Path is Index or "/" 
+// this fuction return a option enum with data type of NamedFile Struct which return result
+#[get("/")]
+fn index()-> Option<NamedFile> {
+    NamedFile::open("static/index.html").ok()
+}
+
+// creat num function for get request
+// where Path is Index or "/"  and <num> is expeting a parametor in url 
+// this fuction return a string
+
+#[get("/<num>")]
+fn num(num:u32)-> String {
+    format!("Number is = {} after add 50 = {}",num ,num + 50)
+}
+
+// creat main function for lucnhing rocket
+
+fn main() {
+    rocket::ignite().mount("/", routes![index,testPost,num]).launch();
+}
